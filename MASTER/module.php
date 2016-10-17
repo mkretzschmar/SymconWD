82caf76403bddac5b509d51484bf73f81f0f28c6
Marek Kretzschmar <mkretzschmar@users.noreply.github.com>
2016-10-17 10:49:15 +0200

    Create module.php

---
+++ MASTER/module.php
@@ -0,0 +1,24 @@
+<?
+  // Klassendefinition
+  class MonitorMaster extends IPSModule {
+ 
+    public function __construct($InstanceID) {
+      parent::__construct($InstanceID);
+    }
+
+    public function Create() {
+      parent::Create();
+    }
+ 
+    public function ApplyChanges() {
+      parent::ApplyChanges();
+    }
+
+    /* 
+     * GIIZ_MonitorReport($id);
+     *
+     */
+    public function MonitorReport() {
+    }
+  }
+?>
