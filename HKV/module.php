7f0fea7e738e9d6c56bc25e411a770b1cb9c26b2
Marek Kretzschmar <mkretzschmar@users.noreply.github.com>
2016-10-17 11:16:24 +0200

    Create module.php

---
+++ HKV/module.php
@@ -0,0 +1,34 @@
+<?
+	class GIIZHKVCollector extends IPSModule {
+  
+		public function Create() {
+			parent::Create();
+			
+			$this->RegisterPropertyString("ReceiveFilter", ".*Hallo.*");
+		}
+    
+		public function ApplyChanges()
+		{
+			parent::ApplyChanges();
+			
+			//Connect to available splitter or create a new one
+			$this->ConnectParent("{6179ED6A-FC31-413C-BB8E-1204150CF376}");
+			
+			//Apply filter
+			$this->SetReceiveDataFilter($this->ReadPropertyString("ReceiveFilter"));
+			
+		}
+		
+		public function ReceiveData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			//Parse and write values to our buffer
+			$this->SetBuffer("Test", utf8_decode($data->Buffer));
+			//Print buffer
+			IPS_LogMessage("IOTest", $this->GetBuffer("Test"));
+			
+		}
+		
+	
+	}
+?>
