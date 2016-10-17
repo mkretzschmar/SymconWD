796cfd78691993e2384b55a9bd6a4702f1d5892e
Marek Kretzschmar <mkretzschmar@users.noreply.github.com>
2016-10-17 11:28:21 +0200

    Update and rename DATABASE/MYSQL/module.php to MYSQL/module.php

---
+++ MYSQL/module.php
@@ -0,0 +1,33 @@
+
+<?
+  // Klassendefinition
+  class GIIZMySQLConnector extends IPSModule {
+ 
+    public function __construct($InstanceID) {
+      parent::__construct($InstanceID);
+    }
+ 
+    public function Create() {
+      parent::Create();
+    }
+ 
+    public function ApplyChanges() {
+      parent::ApplyChanges();
+    }
+ 
+
+        /**
+         * GIIZ_ConnectDatabase($id);
+         */
+        public function ConnectDatabase
+            // Selbsterstellter Code
+        }
+
+        /**
+         * GIIZ_ConnectDatabase($id);
+         */
+        public function DisconnectDatabase
+            // Selbsterstellter Code
+        }
+    }
+?>
