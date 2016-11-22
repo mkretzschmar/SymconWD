9de258b17811799bf55de8daf297c2763fa88fec
Marek Kretzschmar <marekre@fh-zwickau.de>
2016-11-22 11:27:47 +0100

    Update module.php

--- INSTALLER/module.php
+++ INSTALLER/module.php
@@ -1,47 +1,43 @@
 <?
-    // Klassendefinition
-    class GIIZInstaller extends IPSModule {
+// Klassendefinition
+class GIIZInstaller extends IPSModule {
 
-        public function __construct($InstanceID) {
-            parent::__construct($InstanceID);
-
-        }
- 
-
-        public function Create() {
-            parent::Create();
- 
-        }
- 
-        public function ApplyChanges() {
-            parent::ApplyChanges();
-        }
-        
-        /**
-         * GIIZ_GetConfigurationForm($id);
-         */
-        public function GetConfigurationForm()
-		{
-			
-			return '{ "actions": [ { "type": "Label", "label": "The current time is '.date("d.m.y H:i").'" } ] }';
-		
-		}
-    
-        /**
-         *
-         * GIIZ_Install($id);
-         *
-         */
-        public function GetVersion() {
-            echo "0";
-        }
-        /**
-         *
-         * GIIZ_Install($id);
-         *
-         */
-        public function Install() {
-            echo "Installer wird ausgeführt...";
-        }
+    public function __construct($InstanceID) {
+        parent::__construct($InstanceID);
     }
+ 
+    public function Create() {
+        parent::Create();
+    }
+ 
+    public function ApplyChanges() {
+        parent::ApplyChanges();
+    }
+        
+    /**
+     * GIIZ_GetConfigurationForm($id);
+     */
+    public function GetConfigurationForm() {
+		return '{ "actions": [ { "type": "Label", "label": "The current time is '.date("d.m.y H:i").'" } ] }';
+	}
+    
+    /**
+     *
+     * GIIZ_Install($id);
+     *
+     */
+    public function GetVersion() {
+        echo "0";
+    }
+    
+    /**
+     *
+     * GIIZ_Install($id);
+     *
+     */
+    public function Install() {
+        echo "Installer wird ausgeführt...";
+    }
+}
+
 ?>
