22009254c04b684572fb7106a70bde1d4037bfcc
Marek Kretzschmar <marekre@fh-zwickau.de>
2016-11-22 11:26:01 +0100

    Update module.php

--- INSTALLER/module.php
+++ INSTALLER/module.php
@@ -1,26 +1,19 @@
 <?
     // Klassendefinition
     class GIIZInstaller extends IPSModule {
- 
-        // Der Konstruktor des Moduls
-        // Überschreibt den Standard Kontruktor von IPS
+
         public function __construct($InstanceID) {
-            // Diese Zeile nicht löschen
             parent::__construct($InstanceID);
- 
-            // Selbsterstellter Code
+
         }
  
-        // Überschreibt die interne IPS_Create($id) Funktion
+
         public function Create() {
-            // Diese Zeile nicht löschen.
             parent::Create();
  
         }
  
-        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
-            // Diese Zeile nicht löschen
             parent::ApplyChanges();
         }
         
