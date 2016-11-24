81dec1a3074858f576b9d49dadc03fe624e6ca2d
marekre <marekre@WHZ-37365.zw.fh-zwickau.de>
2016-11-24 18:26:09 +0100

    $this->RegisterPropertyBoolean($modul, false);

--- INSTALLER/module.php
+++ INSTALLER/module.php
@@ -1,92 +1,93 @@
 <?php
+
 // Klassendefinition
 class GIIZInstaller extends IPSModule {
 
-    public function __construct($InstanceID) {
-        parent::__construct($InstanceID);
-    }
- 
-    public function Create() {
-        parent::Create();
-    }
- 
-    public function ApplyChanges() {
-        parent::ApplyChanges();
-    }
-        
-    /**
-     * INST_GetConfigurationForm($id);
-     */
-    public function GetConfigurationForm() {
-        
-        $isInstalled = false;
-        $lblText = $isInstalled ? "Aktualisieren" : "Installieren";
-        
-        $modules = array("Watchdog" => "GIIZ Watchdog", "HKV" => "Metrona HKV Datensammler", "Bla" => "Bla");
-        $form = '
+  public function __construct($InstanceID) {
+    parent::__construct($InstanceID);
+  }
+
+  public function Create() {
+    parent::Create();
+  }
+
+  public function ApplyChanges() {
+    parent::ApplyChanges();
+  }
+
+  /**
+   * INST_GetConfigurationForm($id);
+   */
+  public function GetConfigurationForm() {
+
+    $isInstalled = false;
+    $lblText = $isInstalled ? "Aktualisieren" : "Installieren";
+
+    $modules = array("Watchdog" => "GIIZ Watchdog", "HKV" => "Metrona HKV Datensammler", "Bla" => "Bla");
+    $form = '
         { 
         
         "actions": 
             [
-                { "type": "Label", "label": "Letzte Aktualisierung '.date("d.m.y H:i").'" },
+                { "type": "Label", "label": "Letzte Aktualisierung ' . date("d.m.y H:i") . '" },
                 { "type": "Label", "label": " " },
                 { "type": "Label", "label": "Verfügbare Module:" },
                 ';
-        foreach($modules as $modul => $caption) {
-            $this->RegisterPropertyBoolean($modul, false);
-            $form = $form.'{ "type": "CheckBox", "name": "'.$modul.'", "caption": "'.$caption.'" },';
-        }
-        
-        $form = $form.'
-                { "type": "Button", "label": "'.$lblText.'", "onClick": "INST_Install($id);" },
+    foreach ($modules as $modul => $caption) {
+      $this->RegisterPropertyBoolean($modul, false);
+      $form = $form . '{ "type": "CheckBox", "name": "' . $modul . '", "caption": "' . $caption . '" },';
+    }
+
+    $form = $form . '
+                { "type": "Button", "label": "' . $lblText . '", "onClick": "INST_Install($id);" },
                 { "type": "Button", "label": "Erzeuge AutoInstaller-Datei", "onClick": "INST_GenerateAutoInstall($id);" }
             ] }
         
         ';
-        
-        return $form;
+
+    return $form;
+  }
+
+  /**
+   *
+   * INST_GetVersion($id);
+   *
+   */
+  public function GetVersion() {
+    echo "0";
+  }
+
+  /**
+   *
+   * INST_Install($id);
+   *
+   */
+  public function Install() {
+    echo "Installer wird ausgeführt...\nHKV: ".$this->ReadPropertyBoolean("HKV")."";
+    if ($this->ReadPropertyBoolean("HKV")) {
+      InstallMetronaHKV();
     }
+  }
+
+  /**
+   *
+   * INST_GenerateAutoInstall($id);
+   *
+   */
+  public function GenerateAutoInstall() {
+    echo "AutoInstaller wird generiert... \n TODO: Implement";
+  }
+
+  /** PRIVATE FUNCTIONS  */
+  private function InstallWatchdog() {
     
-    /**
-     *
-     * INST_GetVersion($id);
-     *
-     */
-    public function GetVersion() {
-        echo "0";
-    }
-    
-    /**
-     *
-     * INST_Install($id, $mode);
-     *
-     */
-    public function Install() {
-        echo "Installer wird ausgeführt...";
-        
-        InstallMetronaHKV();
-    }
-    
-    /**
-     *
-     * INST_GenerateAutoInstall($id);
-     *
-     */
-    public function GenerateAutoInstall() {
-        echo "AutoInstaller wird generiert... \n TODO: Implement";
-        
-    }
-    
-    
-    /** PRIVATE FUNCTIONS  */
-    private function InstallWatchdog() {
-        
-    }
-    
-    private function InstallMetronaHKV() {
-        echo "Installiere Metrona HKV-Umgebung (TODO: Konfigurator)";
-        MDS_AutoConfig($id);
-    }
+  }
+
+  private function InstallMetronaHKV() {
+    echo "Installiere Metrona HKV-Umgebung (TODO: Konfigurator)";
+    MDS_AutoConfig($id);
+  }
+
 }
 
 ?>
